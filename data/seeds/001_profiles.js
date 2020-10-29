7529ad8b2826c17836e513ea1d1da3724a1a4964
Anthony M Hernandez <55599397+asvka@users.noreply.github.com>
2020-10-28 17:39:35 -0700

    Created more seeds, groomer_profile seeds linked to profiles

--- data/seeds/001_profiles.js
+++ data/seeds/001_profiles.js
@@ -1,13 +1,63 @@
-const faker = require('faker');
+// const faker = require('faker');
 
-const profiles = [...new Array(5)].map((i, idx) => ({
-  id: idx === 0 ? '00ulthapbErVUwVJy4x6' : faker.random.alphaNumeric(20),
-  email: idx === 0 ? 'llama001@maildrop.cc"' : faker.internet.email(),
-  first_name: idx === 0 ? 'Test001' : `${faker.name.firstName()}`,
-  last_name: idx === 0 ? 'User' : `${faker.name.lastName()}`,
-  is_groomer: false,
-}));
-//anything hello
+const profiles = [
+  {
+    id: '00ulthapbErVUwVJy4x6',
+    email: 'llama001@maildrop.cc',
+    first_name: 'Test001',
+    last_name: 'User',
+    is_groomer: true,
+  },
+  {
+    id: '00ultwew80Onb2vOT4x6',
+    email: 'llama002@maildrop.cc',
+    first_name: 'Test002',
+    last_name: 'User',
+    is_groomer: true,
+  },
+  {
+    id: '00ultx74kMUmEW8054x6',
+    email: 'llama003@maildrop.cc',
+    first_name: 'Test003',
+    last_name: 'User',
+    is_groomer: true,
+  },
+  {
+    id: '00ultwqjtqt4VCcS24x6',
+    email: 'llama004@maildrop.cc',
+    first_name: 'Test004',
+    last_name: 'User',
+    is_groomer: true,
+  },
+  {
+    id: '00ultwz1n9ORpNFc04x6',
+    email: 'llama005@maildrop.cc',
+    first_name: 'Test005',
+    last_name: 'User',
+    is_groomer: true,
+  },
+  {
+    id: '00u13omswyZM1xVya4x7',
+    email: 'llama006@maildrop.cc',
+    first_name: 'Test006',
+    last_name: 'User',
+    is_groomer: false,
+  },
+  {
+    id: '00u13ol5x1kmKxVJU4x7',
+    email: 'llama007@maildrop.cc',
+    first_name: 'Test007',
+    last_name: 'User',
+    is_groomer: false,
+  },
+  {
+    id: '00u13oned0U8XP8Mb4x7',
+    email: 'llama001@maildrop.cc',
+    first_name: 'Test008',
+    last_name: 'User',
+    is_groomer: false,
+  },
+];
 exports.seed = function (knex) {
   // Deletes ALL existing entries
   return knex('profiles')
